76787c6373077224b1ef4a433618b0f0a1336395
tom-2810 <tom.poortman@hva.nl>
2023-12-03 21:55:49 +0100

    text border, shadow effect

--- src/lib/components/atoms/Logo.svelte
+++ src/lib/components/atoms/Logo.svelte
@@ -1,9 +1,54 @@
-<span>TP</span>
+<span data-text="TP">TP</span>
 
 <style>
 	span {
+		position: relative;
 		color: #fff;
-		text-shadow: 4px 4px 0px #202020;
-        font-size: 4rem;
+		font-size: 3rem;
+		font-weight: 900;
+		letter-spacing: 0.3px;
+		animation: shift 1s forwards;
+	}
+
+	span::before,
+	span::after {
+		width: 100%;
+		content: attr(data-text);
+		position: absolute;
+		-webkit-text-stroke: 8.5px #202020;
+		font-size: 1em;
+		z-index: -1;
+	}
+
+	span::before {
+		color: #202020;
+		font-weight: 900;
+		animation: shadow 1s forwards;
+	}
+
+	span:after {
+		left: 0;
+	}
+
+	@keyframes shift {
+		from {
+			left: 0;
+			top: 0;
+		}
+		to {
+			left: -0.1em;
+			top: -0.05em;
+		}
+	}
+
+	@keyframes shadow {
+		from {
+			left: 0;
+			top: -0.04em;
+		}
+		to {
+			left: 0.1em;
+			top: 0.015em;
+		}
 	}
 </style>
